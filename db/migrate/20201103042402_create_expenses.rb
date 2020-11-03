dea7792d8bdadc59cffc03ef573e4a63a982ed36
sksn <ps.net26@gmail.com>
2020-11-03 14:26:44 +0900

    Created table 'expenses'

--- db/migrate/20201103042402_create_expenses.rb
+++ db/migrate/20201103042402_create_expenses.rb
@@ -1,9 +1,9 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-      t.string :name
-      t.int :expenses
-      t.text :description
+      t.string :name, limit: 30, null: false
+      t.integer :expenses, limit: 3, null: false
+      t.text :description, limit: 128
 
       t.timestamps
     end
