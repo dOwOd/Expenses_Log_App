19a994b03ce136f5dfd373974de60c9b3d4d94f9
sksn <ps.net26@gmail.com>
2020-11-29 20:30:02 +0900

    comment out 'select_month.js'

--- app/javascript/expenses_log_app/select_month.js
+++ app/javascript/expenses_log_app/select_month.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { css } from '@emotion/css'; 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { css } from '@emotion/css'; 
 
-const selectMonthStyle = css({
-    display: 'inline',
-  });
+// const selectMonthStyle = css({
+//     display: 'inline',
+//   });
 
-document.addEventListener('DOMContentLoaded', () => {
-    let date = {
-        selectDate: new Date()
-    };
+// document.addEventListener('DOMContentLoaded', () => {
+//     let date = {
+//         selectDate: new Date()
+//     };
 
-    let selectYear = date.selectDate.getFullYear();
-    let selectMonth = date.selectDate.getMonth() + 1;
+//     let selectYear = date.selectDate.getFullYear();
+//     let selectMonth = date.selectDate.getMonth() + 1;
 
-    const element = <React.Fragment>
-                        <button type="button">＜</button>
-                        <h1 className={selectMonthStyle}> {selectYear}年{selectMonth}月 </h1>
-                        <button type="button" className={selectMonthStyle}>＞</button>
-                    </React.Fragment>;
+//     const element = <React.Fragment>
+//                         <button type="button">＜</button>
+//                         <h1 className={selectMonthStyle}> {selectYear}年{selectMonth}月 </h1>
+//                         <button type="button" className={selectMonthStyle}>＞</button>
+//                     </React.Fragment>;
     
 
-    ReactDOM.render(element, document.getElementById('month'));
-    // ReactDOM.render(
-    //     React.createElement('div', null, 'select_month!'),
-    //     document.body.appendChild(document.createElement('div')),
-    // );
-});
+//     ReactDOM.render(element, document.getElementById('month'));
+//     // ReactDOM.render(
+//     //     React.createElement('div', null, 'select_month!'),
+//     //     document.body.appendChild(document.createElement('div')),
+//     // );
+// });
