e9d5c9367ec937c318242f863d31ac28ba7f4554
sksn <ps.net26@gmail.com>
2020-12-27 16:28:40 +0900

    Add autocomplete to ‘new expense’ form

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   resources :users
   delete '/users/:group_id/:user_id', to: 'users#destroy', as: :remove
   root to: 'expenses#index'
-  resources :expenses
+  resources :expenses do
+    collection do
+      get "auto_complete" 
+    end 
+  end
   get '/expenses/:select_date/:months', to: 'expenses#index'
   resources :groups do
     resources :groups_users, only: [:new, :create, :edit, :update]
