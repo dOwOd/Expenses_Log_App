f9aa88caab5ba64208cb65f990416084c98ca02b
sksn <ps.net26@gmail.com>
2020-12-29 10:15:54 +0900

    Add button for invitation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@
 
 import 'expenses_log_app/select_month.js';
 import "@emotion/css"
+import '@fortawesome/fontawesome-free/js/all';
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('../suggest')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
