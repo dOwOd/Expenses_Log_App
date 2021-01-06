c3b57d37df6c0b10095e713e2319aa5c6611ab3e
sksn <ps.net26@gmail.com>
2021-01-06 12:34:56 +0900

    Change jQuery to work on first load, reload, and page switch.

--- app/javascript/move_range_value.js
+++ app/javascript/move_range_value.js
@@ -1,6 +1,6 @@
-$(function() {
+//初回読み込み、リロード、ページ切り替えで動く。
+$(document).on('turbolinks:load', function() {
     $('.range-slider').on('input', '.slider-input', function() {
-        　let val = $(this).val();
-            $(this).parent().find('.slider-value').html (this.value + "%"); 
+        $(this).parent().find('.slider-value').html (this.value + "%"); 
     });
-});
+ });
