2e62b5625111f9b889e95d47c74a35a457e8f62d
sksn <ps.net26@gmail.com>
2021-01-10 09:42:16 +0900

    Add validation

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -8,12 +8,19 @@ class UserSettingsController < ApplicationController
     group_user_ids = group_user_ids_params[:group_user_id]
     user_settings_percentage_of_expenses = user_settings_percentage_of_expenses_params[:percentage_of_expenses]
 
-    group_user_ids.each_with_index do |group_user_id, i|
-      @user_setting << UserSetting.find_by(group_user_id: group_user_id)
-      @user_setting[i].percentage_of_expenses = user_settings_percentage_of_expenses[group_user_ids.index(group_user_id)]
-      @user_setting[i].save!
+    user_settings_percentage_of_expenses.map!(&:to_i)
+    if user_settings_percentage_of_expenses.sum == 100
+      group_user_ids.each_with_index do |group_user_id, i|
+        @user_setting << UserSetting.find_by(group_user_id: group_user_id)
+        @user_setting[i].percentage_of_expenses = user_settings_percentage_of_expenses[group_user_ids.index(group_user_id)]
+        @user_setting[i].save!
+      end
+      redirect_to expenses_url, notice: '設定を更新しました。'
+    else
+      flash[:setting_alert] = '割合の合計を100%にしてください。'
+      redirect_to expenses_url
     end
-    redirect_to expenses_url, notice: '設定を更新しました。'
+    
   end 
 
   private
--- app/javascript/move_range_value.js
+++ app/javascript/move_range_value.js
@@ -1,6 +1,6 @@
 //初回読み込み、リロード、ページ切り替えで動く。
 $(document).on('turbolinks:load', function() {
     $('.range-slider').on('input', '.slider-input', function() {
-        $(this).parent().find('.slider-value').html (this.value + "%"); 
+        $(this).parent().find('.slider-value').html (this.value + "%");
     });
  });
