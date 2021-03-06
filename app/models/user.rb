000d86980c75ff53267fd135dc3c4c1a830c9897
sksn <ps.net26@gmail.com>
2021-03-06 11:13:57 +0900

    Fix a bug that prevented the creation of groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
 
   validates :screen_name, presence: true
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
   
   include FriendlyId
   friendly_id :friendly_url
